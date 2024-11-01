5d0303d0925585dbf5cf9f693236f4d829c1a4e9
Wicklet13 <pkalusani2@gmail.com>
2024-11-01 01:20:55 -0400

    Backend DB Fields
    Co-authored-by: yaoyao082008 <yaoyao082008@users.noreply.github.com>
    Co-authored-by: FROZENPOP <FROZENPOP@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from flask import Flask, render_template
+from db import *
+from flask import Flask, render_template, request, session, redirect
+from functools import wraps
+import datetime
+import json
 
-app = Flask(__name__,)
+app = Flask(__name__)
+
+# wrapper function (checks if user is logged in)
+def logged_in_required(func):
+    @wraps(func)
+    def decorator(*args, **kwargs):
+        if session.get("user") is None:
+            return redirect('/')
+        return func(*args, **kwargs)
+
+    return decorator
+
+app.secret_key = "syIyJRgEcuUQ8XI9iG-fA3slxHTBMmk"
+app.permanent_session_lifetime = datetime.timedelta(days=31)
 
 @app.route('/')
 def home():
@@ -8,19 +25,35 @@ def home():
 
 @app.route('/login', methods=["POST"])
 def login():
+    user = json.loads(request.form.get("creds")) # gets user login info
+    session["user"] = get_user(user["email"]) # stores user in browser session
+    session.permanent = True
+    session.modified = True
+
     return ""
 
+@logged_in_required
 @app.route('/account')
 def account():
     return render_template("account.html")
 
+@logged_in_required
 @app.route('/dashboard')
-def search():
+def dashboard():
     return render_template("transactions.html")
 
+@logged_in_required
 @app.route('/analyze')
 def analyze():
     return render_template("analyze.html")
+
+# logout link
+@logged_in_required
+@app.route('/logout', methods=["POST"])
+def logout():
+    session.clear() # clear browser session
+    session.modified = True
+    return ""
 
 if __name__ == "__main__":
     app.run(debug=True)
--- db.py
+++ db.py
@@ -5,3 +5,82 @@ from firebase_admin import firestore, credentials
 creds = credentials.Certificate("firebase_creds.json")
 app = firebase_admin.initialize_app(creds)
 db = firestore.client()
+
+# get user from database
+def get_user(email):
+    ref = db.collection("users").stream()
+    for u in ref:
+        u = u.to_dict()
+        if u["email"] == email:
+            return u # return user if exists
+    new_user(email)
+        
+# new user in database
+def new_user(email):
+    ref = db.collection("users").document(email)
+    data = {
+        "email": email,
+        "income": [],
+        "expenses": [],
+        "balance": None
+        }
+    try: # try to update data
+        ref.update(data)
+    except:
+        #create data 
+        ref.set(data)
+
+def add_income(email, name, type, amt, date):
+    ref = db.collection("users").document(email)
+    data = {
+        'name': name,
+        'type': type,
+        'amt': amt,
+        'date': date
+        }
+    try: # try to update data
+        ref.update({"income": firestore.ArrayUnion([data])})
+    except:
+        #create data 
+        ref.set(data)
+    
+    update_balance(email)
+
+def add_expenses(email, name, type, amt, date):
+    ref = db.collection("users").document(email)
+    data = {
+        'name': name,
+        'type': type,
+        'amt': amt,
+        'date': date
+        }
+    try: # try to update data
+        ref.update({"expenses": firestore.ArrayUnion([data])})
+    except:
+        #create data 
+        ref.set(data)
+    
+    update_balance(email)
+
+def update_balance(email):
+    ref = db.collection("users").document(email)
+    usr = get_user(email)
+    total_income = 0
+    for i in usr["income"]:
+        total_income += i["amt"]
+    
+    total_expense = 0
+    for i in usr["expenses"]:
+        total_expense -= i["amt"]
+
+    bal = total_income + total_expense
+
+    data = {
+        "balance": bal
+        }
+
+    try: # try to update data
+        ref.update(data)
+    except:
+        # create data 
+        ref.set(data)
