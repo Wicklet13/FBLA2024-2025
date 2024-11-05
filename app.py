a3843520c8a21fdb3fc0eb693ca47c96fad95ccf
Wicklet13 <pkalusani2@gmail.com>
2024-11-04 22:07:17 -0500

    User to Dashboard

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from db import *
+from db import get_user
 from flask import Flask, render_template, request, session, redirect
 from functools import wraps
 import datetime
 import json
 
+# Initialize
 app = Flask(__name__)
+app.secret_key = "syIyJRgEcuUQ8XI9iG-fA3slxHTBMmk"
+app.permanent_session_lifetime = datetime.timedelta(days=31)
 
 # wrapper function (checks if user is logged in)
 def logged_in_required(func):
@@ -16,20 +20,17 @@ def logged_in_required(func):
 
     return decorator
 
-app.secret_key = "syIyJRgEcuUQ8XI9iG-fA3slxHTBMmk"
-app.permanent_session_lifetime = datetime.timedelta(days=31)
-
 @app.route('/')
 def home():
     return render_template("index.html")
 
 @app.route('/login', methods=["POST"])
 def login():
+    print("asdf")
     user = json.loads(request.form.get("creds")) # gets user login info
     session["user"] = get_user(user["email"]) # stores user in browser session
     session.permanent = True
     session.modified = True
-
     return ""
 
 @logged_in_required
@@ -40,7 +41,7 @@ def account():
 @logged_in_required
 @app.route('/dashboard')
 def dashboard():
-    return render_template("transactions.html")
+    return render_template("transactions.html", user=session.get("user"))
 
 @logged_in_required
 @app.route('/analyze')
@@ -57,3 +58,4 @@ def logout():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
